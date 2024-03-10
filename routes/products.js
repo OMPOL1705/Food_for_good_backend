aed70acdff655f8c8fa88a0939aef3b6b99634a2
Om <ompol1705@gmail.com>
2024-03-10 11:48:19 +0530

    jefje

--- routes/products.js
+++ routes/products.js
@@ -6,34 +6,23 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/'); // Specify the directory where uploaded images will be stored
+        cb(null, 'uploads/'); 
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname); // Rename the file to avoid duplicates
+        cb(null, file.originalname); 
     }
 });
 
-// Set up multer instance
 const upload = multer({ storage: storage });
-// Route to handle product creation along with image upload
 router.post('/', upload.single('image'), async (req, res) => {
     try {
-        // Log the request body to inspect what data is being received
-        // console.log('Request body:', req.body);
-
-        // Log the uploaded file information
-        // console.log('Uploaded file:', req.file);
-
-        // Extract product data from request body
         const { name, description, price, category } = req.body;
 
-        // Ensure all required fields are present
         if (!name || !description || !price || !category || !req.file) {
             console.log('Missing required fields');
             return res.status(400).json({ error: 'Missing required fields' });
         }
 
-        // Create a new product object with image URL
         const newProduct = new Product({
             name,
             description,
@@ -45,14 +34,11 @@ router.post('/', upload.single('image'), async (req, res) => {
             }
         });
 
-        // Save the new product to the database
         await newProduct.save();
 
-        // Send a success response
         res.status(201).json({ message: 'Product created successfully', product: newProduct });
         
     } catch (error) {
-        // Handle errors
         console.error('Error creating product:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
